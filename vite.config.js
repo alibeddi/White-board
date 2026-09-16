f5647a340d5c434ce7f69e418bc600b3f468f2ba
Ali Beddi <118279441+alibeddi@users.noreply.github.com>
2026-09-16 09:49:31 +0100

    fix: restore vite.config.js content (previous commit accidentally emptied it)

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+export default defineConfig({
+  plugins: [react()],
+});
